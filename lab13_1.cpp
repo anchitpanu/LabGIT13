43555f09e90ee331fd076da9d4d3e9bf77d8937d
anchitpan <anchitpan_p@cmu.ac.th>
2024-01-05 12:46:19 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,35 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+  double sum = 0, sumsquare = 0,wtf = 1,lol = 0;
+  double AM , SD , GM , HM , max=0,min=INFINITY;  
+  
+  int i = 0;
+  
+  while(i < N){
+    sum += A[i];
+    sumsquare += pow(A[i],2); 
+    wtf *= A[i]; 
+    lol += 1/A[i];
+    i++ ;
+  }
+  for(int i=1;i<N;i++){
+    if(A[i]>max) max=A[i];
+    if(A[i]<min) min=A[i];
+  }
+  AM = sum/N;
+  B[0] = AM;
+  
+  SD = sqrt((sumsquare/N)-(pow(AM,2)));
+  B[1] = SD;
+  
+  GM = exp(log(wtf)/N);
+  B[2] = GM;
+  
+  HM = N/lol;
+  B[3] = HM;
+
+  B[4] = max;
+  B[5] = min;
+  }
